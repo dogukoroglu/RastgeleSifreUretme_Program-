51c373f659c360f89de3c8549fffdf13a914cf8e
korog <korog@LAPTOP-GPMKKC5N>
2022-07-01 20:58:02 +0300

    Proje dosyası ekle.

---
+++ RastgeleSifreUretme_Programı/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace RastgeleSifreUretme_Programı
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUzunluk = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtKontrol = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSifre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şifreniz: ";
+            // 
+            // txtUzunluk
+            // 
+            this.txtUzunluk.Location = new System.Drawing.Point(145, 61);
+            this.txtUzunluk.Name = "txtUzunluk";
+            this.txtUzunluk.Size = new System.Drawing.Size(100, 26);
+            this.txtUzunluk.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 68);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 68);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Kontrol Et";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtKontrol
+            // 
+            this.txtKontrol.Location = new System.Drawing.Point(145, 126);
+            this.txtKontrol.Name = "txtKontrol";
+            this.txtKontrol.Size = new System.Drawing.Size(100, 26);
+            this.txtKontrol.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 40);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Lütfen Şifre \r\nUzunluğu Giriniz ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 40);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Oluşturulan Şifreyi\r\nTekrar Giriniz";
+            // 
+            // lblSifre
+            // 
+            this.lblSifre.AutoSize = true;
+            this.lblSifre.Location = new System.Drawing.Point(88, 19);
+            this.lblSifre.Name = "lblSifre";
+            this.lblSifre.Size = new System.Drawing.Size(33, 20);
+            this.lblSifre.TabIndex = 7;
+            this.lblSifre.Text = "null";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 190);
+            this.Controls.Add(this.lblSifre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKontrol);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtUzunluk);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUzunluk;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtKontrol;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSifre;
+    }
+}
+
---
+++ RastgeleSifreUretme_Programı/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RastgeleSifreUretme_Programı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            string karakterler = "QWERTYUIOPĞÜASDFGHJKLŞİZXCVBNMÖÇqwertyuıopğüasdfghjklşizxcvbnmö0123456789";
+            string sonuc = "";
+            int uzunluk = Convert.ToInt16(txtUzunluk.Text);
+
+            
+
+            if(uzunluk >= 8)
+            {
+                for (int i = 0; i <= uzunluk; i++)
+                {
+                    sonuc += karakterler[rnd.Next(karakterler.Length)];
+                }
+                lblSifre.Text = sonuc;
+            }
+            else
+            {
+                MessageBox.Show("Şifreniz 8 karakterden az olamaz, kontrol ediniz!", "Uyarı",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtKontrol.Text == lblSifre.Text)
+            {
+                MessageBox.Show("Şifreniz doğru!", "Bilgi", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+    }
+}
